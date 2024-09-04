7e52cae29b40390302e75d591f3112fc6f273d55
realhardik18 <realhardik18@gmail.com>
2024-09-04 23:03:22 +0530

    deleted unwated code

--- main.py
+++ main.py
@@ -1,22 +1,8 @@
-import requests
 import spotipy
 import requests
-import time
 import lyricsgenius
 from spotipy import SpotifyClientCredentials
 from creds import SPOTIFY_CLIENT_ID,SPOTIFY_CLIENT_SECRET,GENIUS_ACCSESS_TOKEN
-from bs4 import BeautifulSoup
-
-def Top100():
-    data=list()
-    response=requests.get('https://kworb.net/spotify/listeners.html').content
-    soup=BeautifulSoup(response,'html.parser')
-    artists=soup.find_all('tr')
-    for artist in artists[1:101]:
-        artist_id=artist.find('a')['href'][7:29]
-        artist_name=artist.find('a').text
-        data.append([artist_name,artist_id])
-    return data
 
 def ArtistTop10Songs(artist_id):    
     sp=spotipy.Spotify(auth_manager=SpotifyClientCredentials(
@@ -49,24 +35,16 @@ def generate_data(artist_id):
     ))      
     artist_name=sp.artist(artist_id=artist_id)['name'].replace(' ','-')
     songs=ArtistTop10Songs(artist_id=artist_id)
-
     for song in songs:
         lyrics=GetLyrics(song,artist_name)
         if lyrics==False:
             pass
-        else:
-            #print(GetLyrics(song,artist_name))
-            s+=lyrics
-            
-    #print(s)    
+        else:            
+            s+=lyrics            
     with open(f'{artist_name}.txt','w+',encoding='utf-8') as file:
         file.write(s)
 
-def cloud_generator(data):
-    #https://medium.com/@m3redithw/wordclouds-with-python-c287887acc8b
-    pass
-    
-#generate_data(artist_id='1uNFoZAHBGtllmzznpCI3s')
+generate_data(artist_id='5K4W6rqBFWDnAN6FQUkS6x')
 #print('generated')
 
     
